b74a5d5c791bf46e998048cca6f153bf96614fbd
DisabledMallis <mallisiscool@gmail.com>
2022-05-26 19:52:02 -0400

    Fix mismatch + Add cmake configs

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+
+#include <string>
+#include <vector>
+
 #include <ghstl/string>
 #include <ghstl/vector>
 
@@ -6,5 +11,19 @@
 #endif
 
 int main() {
+	std::string std_short_str = "Im short";
+	std::cout << "&std_short_str: " << &std_short_str << std::endl;
+	std::string std_long_str = "Im a longer string that doesn't fit within the 16 byte buffer";
+	std::cout << "&std_long_str: " << &std_long_str << std::endl;
+	std::string std_perfect_str = "I fit perfectly";
+	std::cout << "&std_perfect_str: " << &std_perfect_str << std::endl;
 
+	ghstl::string ghstl_short_str = "Im short";
+	std::cout << "&ghstl_short_str: " << &ghstl_short_str << std::endl;
+	ghstl::string ghstl_long_str = "Im a longer string that doesn't fit within the 16 byte buffer";
+	std::cout << "&ghstl_long_str: " << &ghstl_long_str << std::endl;
+	ghstl::string ghstl_perfect_str = "I fit perfectly";
+	std::cout << "&ghstl_perfect_str: " << &ghstl_perfect_str << std::endl;
+
+	std::cin.get();
 }
